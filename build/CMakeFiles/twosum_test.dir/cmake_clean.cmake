9e92d68861c671a6de3312fbd1b8a4f2c8304b8f
s1111555 <s1111555@mail.yzu.edu.tw>
2026-05-07 14:58:49 +0800

    IK11402-8, add test plans and test cases

---
+++ build/CMakeFiles/twosum_test.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/twosum_test.dir/src/twosum.cpp.obj"
+  "CMakeFiles/twosum_test.dir/src/twosum.cpp.obj.d"
+  "CMakeFiles/twosum_test.dir/tests/twosum_test.cpp.obj"
+  "CMakeFiles/twosum_test.dir/tests/twosum_test.cpp.obj.d"
+  "libtwosum_test.dll.a"
+  "twosum_test.exe"
+  "twosum_test.exe.manifest"
+  "twosum_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/twosum_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/user/github clone/11402_CS351_Project0
+# Build directory: C:/Users/user/github clone/11402_CS351_Project0/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(TwoSumTests "C:/Users/user/github clone/11402_CS351_Project0/build/twosum_test.exe")
+set_tests_properties(TwoSumTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/user/github clone/11402_CS351_Project0/CMakeLists.txt;25;add_test;C:/Users/user/github clone/11402_CS351_Project0/CMakeLists.txt;0;")
+subdirs("_deps/googletest-build")
